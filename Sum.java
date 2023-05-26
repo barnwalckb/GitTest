a1c5fe9b35152625267ac02280033974ab397861
Chandan Barnwal <barnwalckb@gmail.com>
2023-05-26 13:01:51 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,10 @@
+public class Sum {
+
+    static int a = 2;
+            static int b=2;
+    public static void main(String[] args) {
+        System.out.println("sum = " + (a+b));
+        System.out.println("difference = " + (a-b));
+
+    }
+}
